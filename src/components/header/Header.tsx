33073374d04b099bf17564494939372add86a068
Levente <llevente86@gmail.com>
2025-02-15 15:28:36 +0100

    comit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react"
 import DarkModeIcon from "../../Icons/DarkModeIcon"
 import './header.css'
 const Header = () => {
+    const [lightmode, setLightmode] = useState(false)
+    const handlesetlightmod =()=>{
+        setLightmode(!lightmode)
+    }
+    useEffect(() => {
+        document.body.className = `${lightmode? 'light':''}`;
+      }, [lightmode]);
     return (
         <div className='header'>
 
             <h5>Where in the world</h5>
-            <button>{<DarkModeIcon></DarkModeIcon>}Dark Mode</button>
+            <button onClick={handlesetlightmod}>{<DarkModeIcon></DarkModeIcon>}Dark Mode</button>
 
         </div>
     )
